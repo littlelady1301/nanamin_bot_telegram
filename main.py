e3076702fb7b1400cd9a88438f4e4ee1a314686b
littlelady1301 <littlelady3377@gmail.com>
2025-07-07 01:27:23 +0700

    Upload main files

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello, Nanamin Bot is alive."
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return "Webhook received", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
